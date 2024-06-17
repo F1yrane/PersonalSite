951a57ce1a3caa5107b106b2296f8c7c18e114bb
f1yran3 <f1yrane3@gmail.com>
2024-06-17 23:56:27 +0500

    Initial commit

---
+++ src/BlazorApp/Models/SocialIcons.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Models;
+
+public class SocialIcons
+{
+    public string Email { get; set; } = string.Empty;
+    public string GitHub { get; set; } = string.Empty;
+    public string Instagram { get; set; } = string.Empty;
+    public string LinkedIn { get; set; } = string.Empty;
+    public string Telegram { get; set; } = string.Empty;
+}
